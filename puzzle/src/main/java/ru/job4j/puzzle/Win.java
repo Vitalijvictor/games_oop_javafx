257092eee1a027f18f2b1bcce839a45cffaf68f8
VitalijVictor <vitalijvictor@gmail.com>
2021-05-18 11:41:39 +0200

    job4j.puzzle
    7 line error

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -14,8 +14,8 @@ public class Win {
 
     public static boolean rowCheck(int[][] board, int row) {
         boolean rsl = true;
-        for (row = 0; row < board.length; row++){
-            if(board[row][column] != 1){
+        for (int i = 0; i < board.length; i++){
+            if(board[row][i] != 1){
                 rsl = false;
                 break;
             }
@@ -24,8 +24,8 @@ public class Win {
     }
     public static boolean columnCheck(int[][] board, int column) {
         boolean rsl = true;
-        for (column = 0; column < board.length; column++){
-            if(board[row][column] != 1){
+        for (int i = 0; i < board.length; i++){
+            if(board[column][i] != 1){
                 rsl = false;
                 break;
             }
