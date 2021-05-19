aed9a163d4df3499929c0df4e275dbce42bd32a0
VitalijVictor <vitalijvictor@gmail.com>
2021-05-19 20:00:45 +0200

    Win
    final

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,8 +3,9 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+
         for (int i = 0; i < board.length; i++){
-            if (board[i][i] == 1 && (Win.rowCheck(board, i) || Win.columnCheck(board, i))){
+            if ((board[i][i] == 1 && !(Win.rowCheck(board, i) || Win.columnCheck(board, i)))){
                    rsl = false;
                    break;
             }
@@ -25,7 +26,7 @@ public class Win {
     public static boolean columnCheck(int[][] board, int column) {
         boolean rsl = true;
         for (int i = 0; i < board.length; i++){
-            if(board[column][i] != 1){
+            if(board[i][column] != 1){
                 rsl = false;
                 break;
             }
