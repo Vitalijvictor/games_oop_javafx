384bc59d6520869fcf62b52cf37b99bce2b28283
VitalijVictor <vitalijvictor@gmail.com>
2021-08-19 19:48:05 +0200

    2. Каркас шахматной доски [#792 #189867]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,15 +4,35 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.white.BishopWhite;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.Is.isA;
+import static org.junit.Assert.assertEquals;
 
 public class LogicTest {
 
-    @Ignore
     @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void move() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
     }
+
+    @Test(expected = OccupiedCellException.class)
+        public void free() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            Logic logic = new Logic();
+            logic.add(new BishopWhite(Cell.C3));
+            logic.add(new BishopBlack(Cell.D2));
+            logic.move(Cell.C3, Cell.D2);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void findBy() throws FigureNotFoundException, OccupiedCellException,
+            ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.move(Cell.C1, Cell.H6);
+    }
+
 }
